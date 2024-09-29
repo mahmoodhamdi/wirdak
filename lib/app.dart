1cb29fbb6c48a88e171f80c6a14e794fde118c08
mahmoodhamdi <hmdy7486@gmail.com>
2024-09-30 02:27:00 +0300

    Add color constants, text styles, and screen utility setup

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Wirdak',
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    return ScreenUtilInit(
+      designSize: const Size(375, 889),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Wirdak',
+          home: child,
+        );
+      },
     );
   }
 }
