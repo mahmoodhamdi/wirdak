ab045c31091dad294059e36dba3564c4c9520430
mahmoodhamdi <hmdy7486@gmail.com>
2024-10-07 22:38:12 +0300

    Add Awesome Notifications helper and Android configuration

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wirdak/app.dart';
+import 'package:wirdak/core/utils/helpers/notifications_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationsHelper.initializeLocalNotifications();
   runApp(const MyApp());
 }
